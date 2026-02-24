3490a1b011a219401557b50215d569acb9e48e65
Ellen Melo <ellen.melo156@gmail.com>
2026-02-24 17:27:53 -0300

    Componente PostImageCover com Link + Image do Next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ import { SpinLoader } from '../components/SpinLoader';
 import { Suspense } from 'react';
 import { Container } from '../components/Container';
 import { Header } from '../components/Header';
-import Link from 'next/link';
-import Image from 'next/image';
 import { PostHeading } from '../components/PostHeading';
+import { PostCoverImage } from  '../components/PostCoverImage';
 
 export default async function HomePage() {
   return (
@@ -13,16 +12,18 @@ export default async function HomePage() {
       <Header />
 
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
-          <Image
-            className='w-full h-full object-cover object-center group-hover:scale-105 transition'
-            src='/images/passwds.png'
-            width={1200}
-            height={720}
-            alt='Título do post'
-            priority
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{
+            href: '/post/asdfasdf',
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: '/images/passwds.png',
+            alt: 'Alt da imagem',
+            priority: true,
+          }}
+        />
         <div className='flex flex-col gap-4 sm:justify-center'>
           <time
             className='text-slate-600 block text-sm/tight'
