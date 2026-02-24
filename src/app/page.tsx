b42116379546a1f0ed20a0e61d5a38e7db3f16cd
Ellen Melo <ellen.melo156@gmail.com>
2026-02-24 16:56:51 -0300

    Componente FeaturedPost + Tailwind CSS Grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,31 @@ import { SpinLoader } from '../components/SpinLoader';
 import { Suspense } from 'react';
 import { Container } from '../components/Container';
 import { Header } from '../components/Header';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
+          <Image
+            className='group-hover:scale-105 transition'
+            src='/images/passwds.png'
+            width={1200}
+            height={720}
+            alt='Título do post'
+          />
+        </Link>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+          dolorem est dolor porro, doloribus neque, quidem mollitia doloremque,
+          ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam harum blanditiis
+          mollitia?
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
