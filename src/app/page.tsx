918bfe2364a04361d86985762ebc66c53ad7ddf6
Ellen Melo <ellen.melo156@gmail.com>
2026-02-25 08:48:39 -0300

    layout.tsx (root) e Metadata do Next para title e description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
+import { PostFeatured } from '../components/PostFeatured';
 import { PostsList } from '../components/PostsList';
 import { SpinLoader } from '../components/SpinLoader';
 import { Suspense } from 'react';
-import { Container } from '../components/Container';
-import { Header } from '../components/Header';
-import { PostFeatured } from '../components/PostFeatured';
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
@@ -17,10 +13,6 @@ export default async function HomePage() {
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer>
-        <p className='text-6xl font-bold text-center py-8'>Footer</p>
-      </footer>
-    </Container>
+    </>
   );
 }
