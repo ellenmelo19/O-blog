595a3917be26aff1c7a040f9cb6ef3d6f74264c1
Ellen Melo <ellen.melo156@gmail.com>
2026-02-25 13:48:59 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,17 @@ import { PostFeatured } from '../components/PostFeatured';
 import { PostsList } from '../components/PostsList';
 import { SpinLoader } from '../components/SpinLoader';
 import { Suspense } from 'react';
+import { ServerComponent } from '@/src/components/ServerComponent';
+import { ClientComponent } from '@/src/components/ClientComponent';
 
 export default async function HomePage() {
   return (
     <>
+
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+
       <Suspense fallback={<SpinLoader className='min-h-20 mb-16' />}>
         <PostFeatured />
         <PostsList />
