8295ddf77e2c67d4c312676b1cdcee33ffe171ec
Ellen Melo <ellen.melo156@gmail.com>
2026-02-25 08:40:10 -0300

    React cache remove duplicação de chamadas da API ou Repositório

--- src/repositories/post/post-repository.ts
+++ src/repositories/post/post-repository.ts
@@ -1,6 +1,6 @@
 import { PostModel } from "@/src/models/post/post-model";
 
 export interface PostRepository {
-    findAll(): Promise<PostModel[]>; 
     findById(id: string): Promise<PostModel>;
+    findAllPublic(): Promise<PostModel[]>;
 }
