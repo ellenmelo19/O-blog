c7752c701ce7489dc82bd4c3b030758b792c6503
Ellen Melo <ellen.melo156@gmail.com>
2026-02-26 11:58:54 -0300

    ?

---
+++ src/app/exemplo/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { formatHour } from "@/src/utils/format-datetime";
+
+export default async function ExemploDynamicPage() {
+  const hour = formatHour(Date.now());
+
+  return (
+    <main className='min-h-[600px] text-4xl font-bold'>
+      <div>Hora: {hour}</div>
+    </main>
+  );
+}
