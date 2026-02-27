5a1786105f386174c52a306afc319614f056fade
Ellen Melo <ellen.melo156@gmail.com>
2026-02-27 08:03:08 -0300

    Nova diretiva use cache, cacheLife e cacheTag (experimental)

--- src/app/exemplo/[id]/page.tsx
+++ src/app/exemplo/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { revalidateExampleAction } from '@/src/actions/revalidate-example';
-import { formatHour } from '@/src/utils/format-datetime';
+import { formatHourCached } from '@/src/utils/format-datetime';
 
 export default async function ExemploDynamicPage({
   params,
@@ -7,21 +7,12 @@ export default async function ExemploDynamicPage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const hour = formatHour(Date.now());
+  const hour = await formatHourCached();
 
-  const response = await fetch('https://randomuser.me/api/?results=1', {
-    next: {
-      tags: ['randomuser'],
-      revalidate: 30,
-    },
-  });
-  const json = await response.json();
-  const name = json.results[0].name.first;
-
-  return ( 
-    <main className='min-h-[600px] text-4xl font-bold'>
+  return (
+    <main className='min-h-150 text-4xl font-bold'>
       <div>
-        Name: {name} | Hora: {hour} | (ID: {id})
+        Hora: {hour} | (ID: {id})
       </div>
 
       <form className='py-16' action={revalidateExampleAction}>
