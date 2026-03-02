38aa192b8e792bb085a94cc08f24c57fe92326fc
Ellen Melo <ellen.melo156@gmail.com>
2026-03-02 08:53:13 -0300

    Layout e Navegação interna da área administrativa do blog

---
+++ src/app/admin/post/layout.tsx
@@ -0,0 +1,16 @@
+import { MenuAdmin } from '@/src/components/MenuAdmin';
+
+type AdminPostLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function AdminPostLayout({
+  children,
+}: Readonly<AdminPostLayoutProps>) {
+  return (
+    <>
+      <MenuAdmin />
+      {children}
+    </>
+  );
+}
