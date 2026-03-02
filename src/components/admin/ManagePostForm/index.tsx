3dcc080c1803de545c420474794a4e918284cc9a
Ellen Melo <ellen.melo156@gmail.com>
2026-03-02 14:10:27 -0300

    useActionState para Server Actions, Formulários e Estado (state)

--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -4,9 +4,10 @@ import { Button } from '@/src/components/Button';
 import { InputCheckbox } from '@/src/components/InputCheckbox';
 import { InputText } from '@/src/components/InputText';
 import { MarkdownEditor } from '@/src/components/MarkdownEditor';
-import { useState } from 'react';
+import { useActionState, useEffect, useState } from 'react';
 import { ImageUploader } from '../ImageUploader';
 import { PublicPost } from '@/src/dto/post/dto';
+import { createPostAction } from '@/src/actions/post/create-post-action';
 
 type ManagePostFormProps = {
   publicPost?: PublicPost;
@@ -15,8 +16,20 @@ type ManagePostFormProps = {
 export function ManagePostForm({ publicPost }: ManagePostFormProps) {
   const [contentValue, setContentValue] = useState(publicPost?.content || '');
 
+  const initialState = {
+    numero: 0,
+  };
+  const [state, action, isPending] = useActionState(
+    createPostAction,
+    initialState,
+  );
+
+  useEffect(() => {
+    console.log(state.numero);
+  }, [state.numero]);
+
   return (
-    <form action='' className='mb-16'>
+    <form action={action} className='mb-16'>
       <div className='flex flex-col gap-6'>
         <InputText
           labelText='ID'
