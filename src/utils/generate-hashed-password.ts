56ffa5f8b45452746e0e6ed4081fa2312f736e55
Ellen Melo <ellen.melo156@gmail.com>
2026-03-03 11:34:41 -0300

    blablabal

--- src/utils/generate-hashed-password.ts
+++ src/utils/generate-hashed-password.ts
@@ -1,7 +1,7 @@
 import { hashPassword } from '@/src/lib/login/manage-login';
 
 (async () => {
-  const minhaSenha = ''; // NÃO ESQUECER DE APAGAR SUA SENHA DAQUI
+  const minhaSenha = ''; // NÃO ESQUECER DE APAGAR SUA SENHA MANEIRA DAQUI
   const hashDaSuaSenhaEmBase64 = await hashPassword(minhaSenha);
 
   console.log({ hashDaSuaSenhaEmBase64 });
